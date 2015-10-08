93ec9763000c6db19498d1fbfd92a0e8600b8adf
Luca Toma <toma.luca95@gmail.com>
2015-10-08 14:27:44 +0200

    Checkout repository mw-ocg-full tag 0.2

--- install_ocg.sh
+++ install_ocg.sh
@@ -3,6 +3,8 @@ cd /var/lib/ocg
 
 git clone https://github.com/WikiToLearn/mw-ocg-full.git --recursive .
 
+git checkout 0.2
+
 cd /var/lib/ocg/mw-ocg-service
 npm install
 cd /var/lib/ocg/mw-ocg-bundler
