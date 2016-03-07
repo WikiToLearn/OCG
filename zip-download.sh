cd4993b08e3b4c495f4066ba50ca7853257b997c
Alessandro Tundo <a.tundo@campus.unimib.it>
2016-03-07 20:51:02 +0100

    Fixed permission on generated zip files

--- zip-download.sh
+++ zip-download.sh
@@ -17,6 +17,13 @@ fi
 
 PAGE="$2"
 DOMAIN_NAME="$1"
+MY_UID=$(id -u)
+MY_GID=$(id -g)
 
 docker run -v $(pwd)"/test/zip/":/test/zip/ --rm -ti wikitolearn/ocg:new \
  /var/lib/ocg/mw-ocg-bundler/bin/mw-ocg-bundler --php-api http://${DOMAIN_NAME}/ -v -o "/test/zip/${DOMAIN_NAME}-"$(echo $PAGE | sed 's/\//_/g')".zip" -h ${DOMAIN_NAME} --restbase-api http://parsoid.wikitolearn.org/${DOMAIN_NAME}/v3 "$PAGE"
+
+docker run -v $(pwd)"/test/zip/":/test/zip/ --rm -ti wikitolearn/ocg:new \
+ chown $MY_UID:$MY_GID "/test/zip/${DOMAIN_NAME}-"$(echo $PAGE | sed 's/\//_/g')".zip"
+
+
