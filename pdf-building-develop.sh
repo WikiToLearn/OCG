75f87a1f05431a837c920e7e572ce904763c74fa
Cristian Baldi <bld.cris.96@gmail.com>
2016-04-02 20:01:47 +0000

    Small improvement to the pdf-buidling-develop script

--- pdf-building-develop.sh
+++ pdf-building-develop.sh
@@ -12,21 +12,25 @@ fi
 MY_UID=$(id -u)
 MY_GID=$(id -g)
 
-for FULLNAME in $(pwd)"/test/zip/"* ; do
+for FULLNAME in "$(pwd)/test/zip/"* ; do
     FILE_NAME=$(basename $FULLNAME)
     PAGENAME=${FILE_NAME:0:-4}
-    LOG_FILE=$(pwd)"/test/logs/"$PAGENAME".log"
-    echo $PAGENAME
+    LOG_FILE="$(pwd)/test/logs/$PAGENAME.log"
+    echo "Now latexing: $PAGENAME"
 
-    docker run -v "$(pwd)/test/zip/":/test/zip/ \
-    -v "$(pwd)/test/pdf/":/test/pdf/ \
-    -v "$(pwd)/mw-ocg-latexer/bin":/var/lib/ocg/mw-ocg-latexer/bin \
-    -v "$(pwd)/mw-ocg-latexer/lib":/var/lib/ocg/mw-ocg-latexer/lib \
-    -v "$(pwd)/mw-ocg-latexer/tex":/var/lib/ocg/mw-ocg-latexer/tex \
-    -v "$(pwd)/mw-ocg-latexer/package.json":/var/lib/ocg/mw-ocg-latexer/package.json \
-    --rm -ti wikitolearn/ocg:new  \
-    /var/lib/ocg/mw-ocg-latexer/bin/mw-ocg-latexer /test/zip/$PAGENAME.zip -1 -o /test/pdf/$PAGENAME.pdf
+    docker run \
+        -v "$(pwd)/test/zip/":/test/zip/ \
+        -v "$(pwd)/test/pdf/":/test/pdf/ \
+        -v "$(pwd)/mw-ocg-latexer/bin":/var/lib/ocg/mw-ocg-latexer/bin \
+        -v "$(pwd)/mw-ocg-latexer/lib":/var/lib/ocg/mw-ocg-latexer/lib \
+        -v "$(pwd)/mw-ocg-latexer/tex":/var/lib/ocg/mw-ocg-latexer/tex \
+        -v "$(pwd)/mw-ocg-latexer/package.json":/var/lib/ocg/mw-ocg-latexer/package.json \
+        --rm -ti wikitolearn/ocg:new  \
+        /var/lib/ocg/mw-ocg-latexer/bin/mw-ocg-latexer \
+        /test/zip/$PAGENAME.zip -1 \
+        -o /test/pdf/$PAGENAME.pdf | tee $LOG_FILE
 
-    chown $MY_UID:$MY_GID /test/pdf/$PAGENAME.pdf
+    chown $MY_UID:$MY_GID "$(pwd)/test/pdf/$PAGENAME.pdf"
+    chown $MY_UID:$MY_GID "$(pwd)/test/zip/$PAGENAME.zip"
 
 done
