7c62274eecb08fda46fca5d8b94a4ace464e74d6
Toma Luca <toma.luca95@gmail.com>
2016-07-27 18:25:02 +0200

    update in the make-npm-deps-install-dot-sh.py to make thin layers on docker
    Signed-off-by: Toma Luca <toma.luca95@gmail.com>

--- make-npm-deps-install-dot-sh.py
+++ make-npm-deps-install-dot-sh.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python2
 import json
 import os.path
-
-out_file="npm-deps-install.sh"
-npm_deps={}
-npm_install_cmd="npm install -g "
-npm_install_cmd="/docker-npm-install.sh "
-
-if not os.path.exists(out_file):
-    print( "You must have the {} file".format(out_file))
+if not os.path.exists('Dockerfile'):
+    print( "You must have the Dockerfile file")
 else:
-    for file in ['mw-ocg-bundler/package.json', 'mw-ocg-latexer/package.json', 'mw-ocg-service/package.json', 'mw-ocg-texter/package.json']:
-        print ("=> ",file)
-        with open(file) as data_file:
-            data = json.load(data_file)
-            for v in (data['dependencies']):
-                npm_deps[v+"@"+data['dependencies'][v]]=True
-            for v in (data['devDependencies']):
-                npm_deps[v+"@"+data['devDependencies'][v]]=True
+    output = ""
+    outputAppend = True
+    f = open('Dockerfile', 'rb+')
+    for line in f:
+        if line.strip() == "# START_DEP_RESOLVER":
+            outputAppend = False
+            output = output + "# START_DEP_RESOLVER\n"
+            npm_deps = {}
+            for file in ['mw-ocg-bundler/package.json', 'mw-ocg-latexer/package.json', 'mw-ocg-service/package.json', 'mw-ocg-texter/package.json']:
+                with open(file) as data_file:
+                    data = json.load(data_file)
+                    for v in (data['dependencies']):
+                        npm_deps[v+"@"+data['dependencies'][v]]=True
+                    for v in (data['devDependencies']):
+                        npm_deps[v+"@"+data['devDependencies'][v]]=True
+            for dep in npm_deps:
+                output = output + "RUN npm install -g " + dep + "\n"
+        elif line.strip() == "# END_DEP_RESOLVER":
+            outputAppend = True
 
-    for npm_dep in npm_deps:
-        if "mw-ocg-" not in npm_dep:
-            npm_install_cmd = npm_install_cmd + npm_dep + " "
+        if outputAppend:
+            output = output + line.strip() + "\n"
+    f.close()
 
-    print ("Write to {}".format(out_file))
-    target = open(out_file, 'w')
-    target.write("#!/bin/bash\n")
-    target.write("npm update\n")
-    target.write(npm_install_cmd+"\n")
+    target = open('Dockerfile', 'w')
+    target.write(output)
     target.close()
--- npm-deps-install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-npm update
-/docker-npm-install.sh jshint@~2.8.0 prfun@~2.1.1 linewrap@~0.2.1 request@~2.64.0 mocha@~2.3.3 core-js@~0.9.18 icu-bidi@~0.1.5 busboy@0.0.12 jscs@~2.5.0 mime@~1.2.11 npm-travis@~1.0.0 readable-stream@~1.0.33 tmp@~0.0.27 commander@~2.8.1 sprintf-js@0.0.7 colors@~1.1.2 mv@~2.1.1 domino@~1.0.19 rimraf@~2.4.3 sqlite3@~3.1.0 texvcjs@~0.3.0 redis@~0.12.1 bunyan@~1.4.0 gammalatex@~0.0.8 mkdirp@~0.5.0 
